9b4dee01c4b003ea7d42430daed35ff0181cc7d0
Jefferson Santos <jeff.debian2016@gmail.com>
2021-02-11 08:55:36 -0300

    Acrescentando correções para o funcionamento do banco.

--- Treinamentos/AppZoologico/Controllers/AnimalController.cs
+++ Treinamentos/AppZoologico/Controllers/AnimalController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dominio.IRepositorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,18 @@ namespace AppZoologico.Controllers
 {
     public class AnimalController : Controller
     {
+
+        private IAnimal _animal;
+
+        public AnimalController(IAnimal animal)
+        {
+            _animal = animal;
+        }
+
         // GET: Animal
         public ActionResult Index()
         {
-            return View();
+            return View(_animal.ObterTodos());
         }
 
         // GET: Animal/Details/5
